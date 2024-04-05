eb6d6c5a6075b2cac58e96f8cf24616901687e47
Jesper Persson <145968600+JEPPE9103@users.noreply.github.com>
2024-04-05 18:29:40 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,31 @@
-#include <Arduino.h>
+#include <avr/io.h>
 
-// Definiera pin-nummer för LED och knapp
-#define LED_PIN 8
-#define BUTTON_PIN 9
+#define LED_PIN 0 // Pin 8 på Arduino/Freenove
+#define BUTTON_PIN 1 // Pin 9 på Arduino/Freenove
 
-void setup() {
-  // Konfigurera LED-pin som utgång
-  DDRB |= (1 << DDB0); 
+int main() {
+  DDRB |= (1 << LED_PIN); // Sätt LED-pin som utgång
+  DDRB &= ~(1 << BUTTON_PIN); // Sätt knapp-pin som ingång
+  PORTB |= (1 << BUTTON_PIN); // Aktivera den interna pull-up motståndet för knappen
 
-  // Konfigurera knapp-pin som ingång
-  DDRB &= ~(1 << DDB1); 
+  bool ledState = false; // Initialt är LED:n släckt
 
-  // Aktivera den interna pull-up motståndet för knappen
-  PORTB |= (1 << PORTB1); 
-}
+  while (1) {
+    // Läs knappens status
+    bool buttonState = !(PINB & (1 << BUTTON_PIN)); // Om knappen är nedtryckt, läsning ger 0 (false)
 
-void loop() {
-  // Läs knappens status
-  bool buttonState = bit_is_clear(PINB, BUTTON_PIN);
-
-  // Om knappen är nedtryckt, tänd LED:n, annars släck den
-  if (buttonState) {
-    PORTB |= (1 << PORTB0); 
-  } else {
-    PORTB &= ~(1 << PORTB0); 
+    // Om knappen är nedtryckt, ändra LED:ens tillstånd
+    if (buttonState) {
+      ledState = !ledState; // Toggle LED:ens tillstånd
+      if (ledState) {
+        PORTB |= (1 << LED_PIN); // Tänd LED:n
+      } else {
+        PORTB &= ~(1 << LED_PIN); // Släck LED:n
+      }
+      while (buttonState) {
+        buttonState = !(PINB & (1 << BUTTON_PIN)); // Vänta på att knappen släpps
+      }
+    }
   }
+  return 0;
 }
