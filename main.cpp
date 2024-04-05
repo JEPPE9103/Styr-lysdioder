df03ab941476a08e6f07044082f1a201096e7ea2
Jesper Persson <145968600+JEPPE9103@users.noreply.github.com>
2024-04-05 18:21:27 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,13 +6,13 @@
 
 void setup() {
   // Konfigurera LED-pin som utgång
-  DDRB |= (1 << DDB0); // Sätt bit 0 i DDRB till 1 för att göra den till en utgång (LED på pin 8)
+  DDRB |= (1 << DDB0); 
 
   // Konfigurera knapp-pin som ingång
-  DDRB &= ~(1 << DDB1); // Rensa bit 1 i DDRB för att göra den till en ingång (knapp på pin 9)
+  DDRB &= ~(1 << DDB1); 
 
   // Aktivera den interna pull-up motståndet för knappen
-  PORTB |= (1 << PORTB1); // Sätt bit 1 i PORTB till 1 för att aktivera pull-up motstånd (knapp på pin 9)
+  PORTB |= (1 << PORTB1); 
 }
 
 void loop() {
@@ -21,8 +21,8 @@ void loop() {
 
   // Om knappen är nedtryckt, tänd LED:n, annars släck den
   if (buttonState) {
-    PORTB |= (1 << PORTB0); // Sätt bit 0 i PORTB till 1 för att tända LED:n
+    PORTB |= (1 << PORTB0); 
   } else {
-    PORTB &= ~(1 << PORTB0); // Rensa bit 0 i PORTB för att släcka LED:n
+    PORTB &= ~(1 << PORTB0); 
   }
 }
